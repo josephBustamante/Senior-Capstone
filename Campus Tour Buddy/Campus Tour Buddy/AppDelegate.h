8ccac2a2d7bcac95166b2179034a61008239a571
Joseph Bustamante <joseph.bustamante@drake.edu>
2013-03-19 00:00:51 -0500

    Starting commit
    This is just the barebones project. Will be adding on to this with
    branches.

---
+++ Campus Tour Buddy/Campus Tour Buddy/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  Campus Tour Buddy
+//
+//  Created by Joseph Bustamante on 3/18/13.
+//  Copyright (c) 2013 ___FULLUSERNAME___. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
